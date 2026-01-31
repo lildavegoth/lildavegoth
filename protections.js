4cae67bce17a4e747fcd9e6665b9465df096f338
dave <48707271+lildavegoth@users.noreply.github.com>
2026-01-31 18:36:24 +0700

    Add files via upload

--- protections.js
+++ protections.js
@@ -3,7 +3,7 @@
     
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', function() {
-            navigator.serviceWorker.register('/sw.js').then(function() {
+            navigator.serviceWorker.register('./sw.js').then(function() {
             });
         });
     }
