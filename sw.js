f5dd9ece88efa1dbea2229db1980fe741fb81bb1
dave <48707271+lildavegoth@users.noreply.github.com>
2026-02-05 02:25:38 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = 'kakoi-kiraku-app-v1.0.7';
-
+const CACHE_NAME = 'kakoi-kiraku-app-v1.0.9';
 const urlsToCache = [
+    // Root
     '',
     'account.html',
     'auth.js',
@@ -12,6 +12,7 @@ const urlsToCache = [
     'notification.cssjs',
     'protections.js',
     'sw.js',
+    // Pages
     'pages/3-tiles.html',
     'pages/2048.html',
     'pages/appflowy-json-converter.html',
@@ -50,6 +51,7 @@ const urlsToCache = [
     'pages/weather.html',
     'pages/yaml-validator.html',
     'pages/youtube-thumbnail-grabber.html',
+    // Voidgarden
     'pages/voidgarden/adventure.html',
     'pages/voidgarden/character.html',
     'pages/voidgarden/credits.html',
@@ -60,33 +62,135 @@ const urlsToCache = [
 
 self.addEventListener('install', event => {
     self.skipWaiting();
-    event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+    
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache).catch(err => {
+                });
+            })
+    );
 });
 
 self.addEventListener('activate', event => {
     event.waitUntil(
-        caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
     );
     self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
     if (event.request.method !== 'GET') return;
+    
     event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request).then(response => {
-                if (response && response.status === 200 && response.type === 'basic') {
-                    const clone = response.clone();
-                    caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
                 }
-                return response;
-            });
-        }).catch(() => caches.match('index.html'))
+                
+                return fetch(event.request).then(response => {
+                    if (!response || response.status !== 200 || response.type !== 'basic') {
+                        return response;
+                    }
+                    
+                    const responseToCache = response.clone();
+                    
+                    caches.open(CACHE_NAME)
+                        .then(cache => {
+                            cache.put(event.request, responseToCache);
+                        });
+                    
+                    return response;
+                });
+            })
+            .catch(() => {
+                return caches.match('index.html');
+            })
     );
 });
 
+const ALL_FILES_TO_CACHE = [
+    // Root
+    '',
+    'account.html',
+    'auth.js',
+    'browser-homepage.html',
+    'index.html',
+    'manifest.json',
+    'media.css',
+    'navigation.css',
+    'notification.cssjs',
+    'protections.js',
+    'sw.js',
+    // Pages
+    'pages/3-tiles.html',
+    'pages/2048.html',
+    'pages/appflowy-json-converter.html',
+    'pages/app-list.html',
+    'pages/calendar.html',
+    'pages/color-detector.html',
+    'pages/dice-generator.html',
+    'pages/dictionary-manager.html',
+    'pages/giveaway.html',
+    'pages/html-cleaner.html',
+    'pages/html-editor.html',
+    'pages/html-generator.html',
+    'pages/icon-pack.html',
+    'pages/image-to-base64.html',
+    'pages/links-vault.html',
+    'pages/markdown-editor.html',
+    'pages/mockups-generator.html',
+    'pages/movies-gallery.html',
+    'pages/movies-player.html',
+    'pages/music-player.html',
+    'pages/notes-app.html',
+    'pages/o-css-snippets-gallery.html',
+    'pages/o-table-formatting.html',
+    'pages/ocr-tool.html',
+    'pages/posters-gallery.html',
+    'pages/private-storage.html',
+    'pages/projects-task.html',
+    'pages/routine-quest.html',
+    'pages/svg-color-changer.html',
+    'pages/svg-optimizer.html',
+    'pages/svg-rasterizer.html',
+    'pages/svg-to-datauri.html',
+    'pages/typing-game.html',
+    'pages/userscripts-gallery.html',
+    'pages/wallet.html',
+    'pages/weather.html',
+    'pages/yaml-validator.html',
+    'pages/youtube-thumbnail-grabber.html',
+    // Voidgarden
+    'pages/voidgarden/adventure.html',
+    'pages/voidgarden/character.html',
+    'pages/voidgarden/credits.html',
+    'pages/voidgarden/shared-data.js',
+    'pages/voidgarden/shop.html',
+    'pages/voidgarden/voidgarden.html'
+];
+
 self.addEventListener('message', event => {
-    if (event.data.type === 'SKIP_WAITING') {
-        self.skipWaiting();
+    if (event.data.type === 'CACHE_ALL') {
+        event.waitUntil(
+            caches.open(CACHE_NAME).then(cache => {
+                return Promise.all(
+                    ALL_FILES_TO_CACHE.map(url => {
+                        return cache.add(url).catch(() => {});
+                    })
+                );
+            })
+        );
+    } else if (event.data.type === 'SKIP_WAITING') {
+        event.waitUntil(self.skipWaiting());
     }
 });
