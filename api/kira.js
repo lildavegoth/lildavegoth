6b6372a0155c6a6da3dca4c7d040ea993d280d4c
dave <48707271+lildavegoth@users.noreply.github.com>
2026-06-04 08:28:03 +0700

    Update kira.js

--- api/kira.js
+++ api/kira.js
@@ -22,6 +22,49 @@ bot.command("start", async (ctx) => {
 
 bot.command("ping", (ctx) => ctx.reply("pong"));
 
+bot.on(":photo", async (ctx) => {
+  const photos = ctx.message.photo;
+  const fileId = photos[photos.length - 1].file_id;
+  const file = await ctx.api.getFile(fileId);
+  const fileUrl = `https://api.telegram.org/file/bot${process.env.KIRA_TOKEN}/${file.file_path}`;
+
+  const response = await fetch(fileUrl);
+  const buffer = await response.arrayBuffer();
+  const fileName = `${Date.now()}.jpg`;
+
+  const { error } = await supabase.storage
+    .from("images")
+    .upload(fileName, buffer, {
+      contentType: "image/jpeg",
+      upsert: true,
+    });
+
+  if (error) {
+    return ctx.reply("Failed to upload image.");
+  }
+
+  const { data: publicUrlData } = supabase.storage
+    .from("images")
+    .getPublicUrl(fileName);
+
+  const imageUrl = publicUrlData.publicUrl;
+  const encodedUrl = encodeURIComponent(imageUrl);
+
+  const googleLensUrl = `https://lens.google.com/uploadbyurl?url=${encodedUrl}`;
+  const yandexUrl = `https://yandex.com/images/search?rpt=imageview&url=${encodedUrl}`;
+  const startpageUrl = `https://www.startpage.com/do/dsearch?query=${encodedUrl}&cat=images`;
+
+  return ctx.reply("🔍 Search this image:", {
+    reply_markup: {
+      inline_keyboard: [
+        [{ text: "Google Lens", url: googleLensUrl }],
+        [{ text: "Yandex", url: yandexUrl }],
+        [{ text: "Startpage", url: startpageUrl }],
+      ],
+    },
+  });
+});
+
 bot.on("message:text", (ctx) => ctx.reply("You said: " + ctx.message.text));
 
 export async function POST(request) {
