3d6cfe7889a58605008ac281e977ba391b7e3232
dave <48707271+lildavegoth@users.noreply.github.com>
2026-07-23 12:59:32 +0700

    Add files via upload

--- files/fetch/userscripts/longpress-highlight-disabler.user.js
+++ files/fetch/userscripts/longpress-highlight-disabler.user.js
@@ -1,16 +1,17 @@
 // ==UserScript==
 // @name         Long Press Highlight Disabler
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Disables the blue highlight on long press for URLs and elements with embedded URLs
 // @author       lildavegoth
 // @match        *://*/*
-// @icon        https://raw.githubusercontent.com/lildavegoth/lildavegoth/refs/heads/homepage/favicon.ico
+// @icon         https://raw.githubusercontent.com/lildavegoth/lildavegoth/refs/heads/homepage/favicon.ico
 // @grant        none
 // ==/UserScript==
 
 (function() {
+    'use strict';
     var style = document.createElement('style');
-    style.textContent = '* { -webkit-tap-highlight-color: transparent; -webkit-touch-callout: none; }';
+    style.textContent = '* { -webkit-tap-highlight-color: transparent !important; -webkit-touch-callout: none !important; }';
     document.head.appendChild(style);
 })();
--- files/fetch/userscripts/selected-text-color-changer.user.js
+++ files/fetch/userscripts/selected-text-color-changer.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Selected Text Color Changer
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Change selected text color to Kakoi Kiraku Accent Color
 // @author       lildavegoth
 // @match        *://*/*
-// @icon        https://raw.githubusercontent.com/lildavegoth/lildavegoth/refs/heads/homepage/favicon.ico
+// @icon         https://raw.githubusercontent.com/lildavegoth/lildavegoth/refs/heads/homepage/favicon.ico
 // @grant        none
 // @run-at       document-start
 // ==/UserScript==
@@ -13,8 +13,8 @@
 (function() {
     'use strict';
 
-    const style = document.createElement('style');
-    style.textContent = '::selection { color: #C1FC32 !important; } ::-moz-selection { color: #C1FC32 !important; }';
+    var style = document.createElement('style');
+    style.textContent = '::selection { color: #C1FC32 !important; } ::-moz-selection { color: #C1FC32 !important; } ::-webkit-selection { color: #C1FC32 !important; }';
 
     if (document.head) {
         document.head.appendChild(style);
